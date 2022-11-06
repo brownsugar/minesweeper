59a1ccee64e0e886d27d4006e0d2c43aed385004
Lay <brownsugar.idv.tw@gmail.com>
2022-11-07 02:11:32 +0800

    feat: adjust initial mines placement & auto reveal adjacent grids

--- src/composables/core.ts
+++ src/composables/core.ts
@@ -11,19 +11,41 @@ const gridDataDefault = {
   adjacent: 0
 }
 type gridDataT = typeof gridDataDefault
-
-export const gridData = ref<gridDataT[]>([])
-
 const gridTotal = computed(() => rows.value * cols.value)
 
+// Grids
+export const gridData = ref<gridDataT[]>([])
+export const flagGrid = (index: number, value: boolean = undefined) => {
+  const grid = gridData.value[index]
+  if (grid.revealed) {
+    return
+  }
+  if (value === undefined) {
+    grid.flagged = !grid.flagged
+    return
+  }
+  grid.flagged = value
+}
+export const revealGrid = (index: number) => {
+  const grid = gridData.value[index]
+  if (grid.flagged || grid.revealed) {
+    return
+  }
+  grid.revealed = true
+  if (!grid.isMine && grid.adjacent === 0) {
+    revealAdjacent(index)
+  }
+}
+
+// Game
 export const initGame = () => {
   gridData.value = Array<gridDataT>(gridTotal.value)
     .fill(null)
     .map(() => ({ ...gridDataDefault }))
 }
-
 export const placeMines = (initialIndex: number) => {
-  const minesIndex = getMinesIndex(initialIndex)
+  const adjacentIndexes = getAdjacentIndexes(initialIndex)
+  const minesIndex = getMinesIndex(adjacentIndexes)
   gridData.value
     .map((grid, i) => {
       if (minesIndex.includes(i)) {
@@ -31,26 +53,59 @@ export const placeMines = (initialIndex: number) => {
       }
       return grid
     })
-    .forEach((grid, index, array) => {
-      const found = seekSurrondingGrids(index, array)
+    .forEach((grid, index) => {
+      const found = countAdjacent(index)
       grid.adjacent = found
     })
 }
-
-const getMinesIndex = (initialIndex: number) => {
+const getMinesIndex = (adjacentIndexes: number[]) => {
   const result: number[] = []
   while (result.length < mines.value) {
     const index = getRandomNumber(0, gridTotal.value - 1)
-    if (index !== initialIndex && !result.includes(index)) {
+    if (!adjacentIndexes.includes(index) && !result.includes(index)) {
       result.push(index)
     }
   }
   return result
 }
+export const isGameComplete = () => {
+  const revealed = gridData.value.filter(grid => grid.revealed)
+  return revealed.length === gridTotal.value - mines.value
+}
 
-const seekSurrondingGrids = (index: number, data: gridDataT[]) => {
-  const { row, col } = indexToCoord(index)
+// Finders
+const getAdjacentIndexes = (index: number) => {
+  const indexes: number[] = []
+  seekAdjacentGrids(index, (_, i) => {
+    indexes.push(i)
+  }, true)
+  return indexes
+}
+const countAdjacent = (index: number) => {
   let found = 0
+  seekAdjacentGrids(index, (grid) => {
+    if (grid.isMine) {
+      found++
+    }
+  })
+  return found
+}
+const revealAdjacent = (index: number) => {
+  seekAdjacentGrids(index, (grid, i) => {
+    if (!grid.isMine && !grid.revealed) {
+      grid.revealed = true
+      if (grid.adjacent === 0) {
+        revealAdjacent(i)
+      }
+    }
+  })
+}
+const seekAdjacentGrids = (
+  index: number,
+  callback: (grid: gridDataT, index: number) => void,
+  includeSelf = false
+) => {
+  const { row, col } = indexToCoord(index)
   for (let i = row - 1; i <= row + 1; i++) {
     if (i < 1 || i > rows.value) {
       continue
@@ -58,36 +113,25 @@ const seekSurrondingGrids = (index: number, data: gridDataT[]) => {
     for (let j = col - 1; j <= col + 1; j++) {
       if (
         j < 1 || j > cols.value ||
-        (i === row && j === col)
+        (!includeSelf && i === row && j === col)
       ) {
         continue
       }
       const index = coordToIndex(i, j)
-      const isMine = data[index].isMine
-      if (isMine) {
-        found++
-      }
+      callback(gridData.value[index], index)
     }
   }
-  return found
-}
-
-export const isGameComplete = () => {
-  const revealed = gridData.value.filter(grid => grid.revealed)
-  return revealed.length === gridTotal.value - mines.value
 }
 
 export const coordToIndex = (row: number, col: number) => {
   return (row - 1) * cols.value + col - 1
 }
-
 export const indexToCoord = (index: number) => {
   return {
     row: Math.floor(index / cols.value) + 1,
     col: (index % cols.value) + 1
   }
 }
-
 const getRandomNumber = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
