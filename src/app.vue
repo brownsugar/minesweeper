7fd345f5f3999b2253ae407672c9d4d07f8fbe15
Lay <brownsugar.idv.tw@gmail.com>
2022-11-07 04:38:54 +0800

    feat: add page meta & footer

--- src/app.vue
+++ src/app.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="wrap">
     <TheMinesweeper />
+    <TheFooter />
   </div>
 </template>
 
 <script lang="ts" setup>
+const icon = '/icon.png'
+const title = 'Minesweeper with Vue 3 & TypeScript'
+const description = 'Make the classic game with Vue 3 & TypeScript.'
+const url = 'https://minesweeper-with-vue.vercel.app'
+const appBarColor = '#8E9775'
+
 useHead({
+  title,
   link: [
+    { rel: 'icon', type: 'image/x-icon', href: icon },
+    { rel: 'apple-touch-icon', href: icon },
     {
       rel: 'preconnect',
       href: 'https://fonts.googleapis.com'
@@ -20,6 +30,31 @@ useHead({
       rel: 'stylesheet',
       href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap'
     }
+  ],
+  meta: [
+    { name: 'theme-color', content: appBarColor },
+    { name: 'apple-mobile-web-app-status-bar-style', content: appBarColor },
+    {
+      name: 'description',
+      content: description
+    },
+    {
+      property: 'og:title',
+      content: title
+    },
+    { property: 'og:type', content: 'website' },
+    {
+      property: 'og:locale',
+      content: 'en'
+    },
+    {
+      property: 'og:url',
+      content: url
+    },
+    {
+      property: 'og:description',
+      content: description
+    }
   ]
 })
 </script>
@@ -27,6 +62,7 @@ useHead({
 <style lang="scss" scoped>
 .wrap {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   height: 100vh;
---
+++ src/components/the-footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer>
+    <a
+      href="https://github.com/brownsugar/minesweeper"
+      target="_blank"
+      rel="noopener noreferrer"
+    >View source code on GitHub</a>
+  </footer>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+  name: 'TheFooter'
+})
+</script>
+
+<style lang="scss" scoped>
+footer {
+  margin-top: 50px;
+}
+a {
+  color: #8E9775;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+}
+</style>
